73f38db56bd3e5c9fda840d6f23d06cd89bbeb4b
BobrImperator <lordboberro@gmail.com>
2016-04-03 17:51:10 +0200

    Changing Coworker model

--- app/models/coworker.js
+++ app/models/coworker.js
@@ -1,5 +1,18 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  email: DS.attr('string'),
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  avatarUrl: DS.attr('string'),
+
+  fullName: Ember.computed('firstName', 'lastName'
+function() {
+  return '${this.get('firstName')} ${this.get('lastName')}';
+
+}),
+  nickname: Ember.computed('email', function(){
+    return this.get('email').split('@')[0];
+  })
+
 });
