1ca135cc71d58e3f47cdf6b5cf2c68b505af692a
BobrImperator <lordboberro@gmail.com>
2016-04-07 18:40:07 +0200

    Kudo-new# Please enter the commit message for your changes. Lines starting

--- app/controllers/dashboard/coworker/kudo-new.js
+++ app/controllers/dashboard/coworker/kudo-new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  kudo: ember.computed( function() {
+    return this._newKudo();
+  }),
+  actions: {
+    create() {
+      let kudo = this.get('kudo'),
+      coworker = this.store.peekRecord('coworker',this.model.id);
+
+      kudo.set('coworker' , coworker);
+      kudo.save();
+      this.set('kudo', this._newKudo());
+    }
+  },
+
+  _newKudo() {
+    return this.store.createRecord('kudo' , {value: 1, comment: ' ;-)'});
+  }
 });
