b1a53a839befe35a0978b3fbc96937400cc10107
BobrImperator <lordboberro@gmail.com>
2016-04-07 18:31:26 +0200

    Controller and Route Adding kudos to coworkers

---
+++ tests/unit/routes/dashboard/coworker/kudo-new-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dashboard/coworker/kudo-new', 'Unit | Route | dashboard/coworker/kudo new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
