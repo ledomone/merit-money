9c4d7c218f2680715a61be065c273314347431ad
BobrImperator <lordboberro@gmail.com>
2016-04-07 16:57:02 +0200

    Computing properties for coworkers

--- app/models/coworker.js
+++ app/models/coworker.js
@@ -6,7 +6,7 @@ export default DS.Model.extend({
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
   avatarUrl: DS.attr('string'),
-  
+
   kudos: DS.hasMany('kudo', {async: true}),
 
   fullName: Ember.computed('firstName', 'lastName', function() {
@@ -15,5 +15,10 @@ export default DS.Model.extend({
 
   nickname: Ember.computed('email', function() {
     return this.get('email').split('@')[0];
+  }),
+
+  kudos_total_value: Ember.computed('kudos.@each.value', function() {
+
+    return this.get('kudos').reduce((a, b) => a + b.get('value'), 0);
   })
 });
